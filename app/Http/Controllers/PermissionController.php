e40094d2c6ee84fc7a25841b1750970d8df17242
SeaMengSrun1 <sronsea123@egmail.com>
2024-02-13 14:08:30 +0700

    role-permission and permission migrate

---
+++ app/Http/Controllers/PermissionController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Permission;
+
+class PermissionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return Permission::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'fullname' => "required|string",
+            'acronym' => 'required|string|max:12',
+            'description' => 'required|string'
+        ]);
+        Permission::create($validated);
+
+        return response()->noContent();
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $specified = Permission::find($id);
+        return response()->json($specified, 200);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $validated = $request->validate([
+            'fullname' => "required|string",
+            'acronym' => 'required|string|max:12',
+            'description' => 'required|string'
+        ]);
+        $findrole = Permission::find($id);
+        $findrole->update($validated);
+
+        return response()->noContent();
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $finds = Permission::find($id);
+        $finds->delete();
+    }
+}
---
+++ database/migrations/2024_02_13_023540_create_permission_role_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use App\Models\Role;
+use App\Models\Permission;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('permission_role', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Role::class)->constrained()->cascadeOnDelete();
+            $table->foreignIdFor(Permission::class)->constrained()->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('permission_role');
+    }
+};
